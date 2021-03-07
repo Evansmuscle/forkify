d31d3f301e7115eaf872f17bbd974d78afbfb1ef
Evansmuscle <katokorkmaz@gmail.com>
2021-03-07 23:51:43 +0300

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd111a55d-b035-4dd1-a99d-64a1347bf478';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/resultsView.js
@@ -0,0 +1,20 @@
+import View from './view.js';
+import previewView from './previewView.js';
+import icons from 'url:../../img/icons.svg'; // Parcel 2
+
+class ResultsView extends View {
+  _parentElement = document.querySelector('.results');
+  _errorMessage = 'No recipe found for your search!';
+  _successMessage = '';
+
+  addHandlerRender(handler) {
+    window.addEventListener('load', handler);
+  }
+
+  _generateMarkup() {
+    console.log(this._data);
+    return this._data.map(result => previewView.render(result, false)).join('');
+  }
+}
+
+export default new ResultsView();
